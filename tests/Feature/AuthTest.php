637977607838dd8e7db45e3c97fac30466f27dd0
Степан Райкевич <stepanraikevich@gmail.com>
2023-06-30 17:21:28 +0300

    add auth test

---
+++ tests/Feature/AuthTest.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Tests\Feature;
+
+
+use App\Models\Desk;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Symfony\Component\HttpFoundation\Response;
+use Tests\TestCase;
+
+class AuthTest extends TestCase
+{
+
+    public function test_index(): void
+    {
+        $response = $this->get('/api/V1/user', [
+            'Accept' => 'application/json'
+        ]);
+
+        $response->assertStatus(Response::HTTP_UNAUTHORIZED);
+    }
+
+    public function test_registerSuccess(): void
+    {
+        $newUser = Desk::query()->latest();
+
+        $response = $this->post('/api/V1/register',[
+            "name" => $newUser['name'],
+            "email"=>"test@gmail.com",
+            "password"=>"123"
+        ], [
+            'Accept' => 'application/json'
+        ]);
+
+        $response->assertStatus(Response::HTTP_CREATED);
+    }
+
+    public function test_loginSuccess(): void
+    {
+        $response = $this->post('/api/V1/login',[
+            "email"=>"test@gmail.com",
+            "password"=>"123"
+        ], [
+            'Accept' => 'application/json'
+        ]);
+
+        $response->assertStatus(Response::HTTP_OK);
+    }
+
+
+    //пофиксить запрос существующей в бд почты и добавить удаление юзера
+}
